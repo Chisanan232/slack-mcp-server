c2af9282620d294cb187d527a7d84df9d5fa79f4
Chisanan232 <chi10211201@cycu.org.tw>
2025-06-25 09:58:03 +0800

    ➕ Add command line option value setting for the new step about loading the environment variable values from the *.env* file or command line option setting for the Slack webhook server shell script.

--- scripts/docker/run-slack-webhook-server.sh
+++ scripts/docker/run-slack-webhook-server.sh
@@ -21,11 +21,26 @@ if [ -n "${SLACK_WEBHOOK_LOG_LEVEL}" ]; then
   CMD_ARGS+=(--log-level "${SLACK_WEBHOOK_LOG_LEVEL}")
 fi
 
+# SLACK_BOT_TOKEN: Slack bot token
+if [ -n "${SLACK_BOT_TOKEN}" ]; then
+  CMD_ARGS+=(--slack-token "${SLACK_BOT_TOKEN}")
+fi
+
+# ENV_FILE: Path to .env file
+if [ -n "${SLACK_WEBHOOK_ENV_FILE}" ]; then
+  CMD_ARGS+=(--env-file "${SLACK_WEBHOOK_ENV_FILE}")
+fi
+
+# NO_ENV_FILE: Disable .env file loading
+if [ -n "${SLACK_WEBHOOK_NO_ENV_FILE}" ] && [ "${SLACK_WEBHOOK_NO_ENV_FILE}" = "true" ]; then
+  CMD_ARGS+=(--no-env-file)
+fi
+
 # Print the command that will be executed
 echo "Starting Slack webhook server with arguments: ${CMD_ARGS[@]}"
 # Only print debug command information if log level is debug (case insensitive)
 if [ -n "${SLACK_WEBHOOK_LOG_LEVEL}" ] && [ "$(echo ${SLACK_WEBHOOK_LOG_LEVEL} | tr '[:upper:]' '[:lower:]')" == "debug" ]; then
-  echo "[DEBUG] Run the Slack webhook server with command: python -m slack_mcp.slack_server ${CMD_ARGS[@]}"
+  echo "[DEBUG] Run the Slack webhook server with command: uv run slack-events-server ${CMD_ARGS[@]}"
 fi
 
 # Execute the entry point with the collected arguments
