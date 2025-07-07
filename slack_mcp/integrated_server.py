a14a2ae7b5a4d409383ad84d0725489d48991063
Chisanan232 <chi10211201@cycu.org.tw>
2025-07-07 16:45:28 +0800

    ✏️ Adjust the imports to be more clear.

--- slack_mcp/integrated_server.py
+++ slack_mcp/integrated_server.py
@@ -11,9 +11,8 @@ from typing import Final, Optional
 
 from fastapi import FastAPI
 
-from slack_mcp.webhook.server import create_slack_app, initialize_slack_client
-
-from slack_mcp.mcp.server import mcp as _server_instance
+from .webhook.server import create_slack_app, initialize_slack_client
+from .mcp.server import mcp as _server_instance
 
 __all__: list[str] = [
     "create_integrated_app",
