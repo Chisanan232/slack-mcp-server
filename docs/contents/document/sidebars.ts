74c226f18326c961760415ca569aeae071e80bd3
Chisanan232 <chi10211201@cycu.org.tw>
2025-09-17 16:22:18 +0800

    ➕ Add the sidebar setting for the document navigation.

---
+++ docs/contents/document/sidebars.ts
@@ -0,0 +1,62 @@
+import type {SidebarsConfig} from '@docusaurus/plugin-content-docs';
+
+/**
+ * Sidebar for the Docs section
+ */
+const sidebars: SidebarsConfig = {
+  docs: [
+    'introduction',
+    {
+      type: 'category',
+      label: '🤟 Quickly Start',
+      collapsed: false,
+      items: [
+        'quick-start/quick-start',
+        'quick-start/requirements',
+        'quick-start/installation',
+        'quick-start/how-to-run',
+      ],
+    },
+    {
+      type: 'category',
+      label: '🧑‍💻 API References',
+      items: [
+        'api-references/api-references',
+        {
+          type: 'category',
+          label: 'Web APIs',
+          items: [
+            'api-references/web-apis/web-apis',
+            'api-references/web-apis/web-api-health-check',
+          ],
+        },
+        {
+          type: 'category',
+          label: 'MCP Server APIs',
+          items: [
+            'api-references/mcp-apis/mcp-apis',
+            'api-references/mcp-apis/slack-post-message-mcp-api',
+            'api-references/mcp-apis/slack-read-channel-messages-mcp-api',
+            'api-references/mcp-apis/slack-read-thread-messages-mcp-api',
+            'api-references/mcp-apis/slack-thread-reply-mcp-api',
+            'api-references/mcp-apis/slack-read-emojis-mcp-api',
+            'api-references/mcp-apis/slack-add-reactions-mcp-api',
+          ],
+        },
+      ],
+    },
+    {
+      type: 'category',
+      label: '👋 Welcome to contribute',
+      items: [
+        'contribute/contribute',
+        'contribute/report-bug',
+        'contribute/request-changes',
+        'contribute/discuss',
+      ],
+    },
+    'changelog',
+  ],
+};
+
+export default sidebars;
